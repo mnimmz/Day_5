557f066711339d85ca304e1c8ebdd3b709c9de23
mnimmz <nimmons92@gmail.com>
2017-07-04 17:51:23 -0400

    Day 5 homework

---
+++ Day_5/src/projects/ClassVariable.java
@@ -0,0 +1,16 @@
+package projects;
+
+public class ClassVariable {
+	
+	static String name = "Morgan";
+	static String number = "561-750-3922";
+	static String city = "Boca Raton";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print(name + " " + number + " " + city);
+
+	}
+
+}
